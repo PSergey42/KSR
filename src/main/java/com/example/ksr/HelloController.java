b3d4ee76aa91a89ef0486ba52b9c4bb5a466505c
Sergey Polyakov <serega.uz@mail.ru>
2021-12-17 19:49:32 +0400

    da3

--- src/main/java/com/example/ksr/HelloController.java
+++ src/main/java/com/example/ksr/HelloController.java
@@ -9,15 +9,13 @@ public class HelloController extends Parent {
     @FXML
     private WebView webViewMap;
 
-    private WebEngine webEngine;
-    private boolean ready;
 
     @FXML
     void initialize() throws MalformedURLException {
-        File file = new File("D:/Programs/KSR/src/main/resources/com/example/ksr/index.html");
+        /*File file = new File("D:/Programs/KSR/src/main/resources/com/example/ksr/index.html");
         URL url= file.toURI().toURL();
-        webViewMap.getEngine().load(url.toString());
-
+        webViewMap.getEngine().load(url.toString());*/
+        GoogleMap googleMap = new GoogleMap(webViewMap);
 
     }
 
